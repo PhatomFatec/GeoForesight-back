072f541e06e053ef2644546b881194fe15c8126d
Lucas Guiraldelli <lucas.guiraldelli@outlook.com>
2023-10-13 18:04:57 +0000

    query igor

--- myapp/app.py
+++ myapp/app.py
@@ -33,6 +33,7 @@ db = SQLAlchemy(app)
 ##### Define Functions ######
 #############################
 
+
 def mongo_connection():
     uri = "mongodb+srv://phantom:<password>@cluster0.yxkoek8.mongodb.net/?retryWrites=true&w=majority"
     client = MongoClient(uri)
@@ -165,7 +166,6 @@ with app.app_context():
     db.create_all()
 
 
-
 #############################
 ###### login routes #########
 #############################
@@ -204,7 +204,6 @@ def login_mongo():
     # Recupere o hash da senha do documento MongoDB
     hashed_password = user.get("senha")
 
-
     if user and bcrypt.checkpw(senha.encode('utf-8'), hashed_password.encode('utf-8')):
         # Credenciais válidas, crie um token JWT
         access_token = create_access_token(identity=email)
@@ -213,10 +212,9 @@ def login_mongo():
         return jsonify({'message': 'Credenciais inválidas.'}), 401
         # Se as credenciais não forem válidas (email incorreto, senha incorreta ou ambos),
         # retorna uma resposta JSON com uma mensagem de "Credenciais inválidas" e um código de status HTTP 401 (Não Autorizado).
-    
+
     # Feche a conexão com o MongoDB
     client.close()
-
 
 
 #############################
@@ -318,17 +316,108 @@ def consulta_dinamica():
         return jsonify({'error': 'Ocorreu um erro no processamento da solicitação.'}), 500
 
 
-@app.route('/consulta_dinamica/', methods=['POST'])
-@jwt_required()
-def consulta_dinamica():
-    current_user = get_jwt_identity()
-    data = request.json
+# # nova consulta
+# @app.route('/consulta_dinamica/', methods=['POST'])
+# @jwt_required()
+# def consulta_dinamica():
+#     current_user = get_jwt_identity()
+#     data = request.json
 
-# adicionar os seguintes campos no filtro da query dinamica:
-    # Solo 
-    # Clima
-    # Ciclo do cultivo
-    # Estado
+#     # adicionar os seguintes campos no filtro da query dinamica:
+#     # Solo
+#     # Clima
+#     # Ciclo do cultivo
+#     # Estado
+
+#     print(data)
+#     try:
+
+#         query = ''' SELECT 
+#                         sub.ref_bacen,
+#                         sub.nu_identificador,
+#                         string_agg(concat(sub.latitude, ',', sub.longitude), '/') as coordenadas,
+#                         oce.inicio_plantio,
+#                         oce.final_plantio,
+#                         oce.data_vencimento,
+#                         oce.inicio_colheita,
+#                         oce.final_colheita,
+#                         irrigacao.descricao as descricao_irrigacao,
+#                         ciclo_producao.descricao as descricao_producao,
+#                         grao.descricao as descricao_grao
+#                     FROM (
+#                         SELECT 
+#                             glebas.ref_bacen,
+#                             glebas.nu_identificador,
+#                             glebas.latitude,
+#                             glebas.longitude,
+#                             glebas.nu_ponto
+#                         FROM 
+#                             glebas
+#                             JOIN operacao_credito_estadual ON glebas.ref_bacen = operacao_credito_estadual.ref_bacen
+#                             JOIN irrigacao ON irrigacao.idirrigacao = operacao_credito_estadual.idirrigacao
+#                             JOIN grao ON grao.idgrao = operacao_credito_estadual.idgrao
+#                             JOIN ciclo_producao ON ciclo_producao.idciclo = operacao_credito_estadual.idciclo
+#                         WHERE 
+#                             1=1
+#                         ORDER BY 
+#                             glebas.nu_ponto ASC
+#                     ) as sub
+#                     JOIN operacao_credito_estadual oce ON sub.ref_bacen = oce.ref_bacen
+#                     JOIN irrigacao ON irrigacao.idirrigacao = oce.idirrigacao
+#                     JOIN grao ON grao.idgrao = oce.idgrao
+#                     JOIN ciclo_producao ON ciclo_producao.idciclo = oce.idciclo
+#                     WHERE 1=1
+#                     GROUP BY 
+#                         sub.ref_bacen,
+#                         oce.inicio_plantio, 
+#                         oce.final_plantio,
+#                         oce.data_vencimento,
+#                         oce.inicio_colheita,
+#                         oce.final_colheita,
+#                         irrigacao.descricao,
+#                         ciclo_producao.descricao ,
+#                         grao.descricao,
+#                         sub.nu_identificador
+#                     WHERE 
+#                         1=1 '''
+
+#         # Criar uma conexão com o banco de dados
+#         # Substitua pela sua string de conexão
+#         engine = create_engine(
+#             'postgresql://postgres:123@localhost/geoforesight')
+#         conn = engine.connect()
+
+#         # Executar a query
+#         resultados = conn.execute(text(query)).fetchall()
+
+#         # Montar a lista de resultados
+#         lista_resultados = []
+#         for resultado in resultados:
+#             resultado_dict = {
+#                 "ref_bacen": resultado.ref_bacen,
+#                 "nu_ordem": resultado.nu_ordem,
+#                 "coordenadas": resultado.coordenadas,
+#                 "inicio_plantio": resultado.inicio_plantio,
+#                 "final_plantio": resultado.final_plantio,
+#                 "data_liberacao": resultado.data_liberacao,
+#                 "data_vencimento": resultado.data_vencimento,
+#                 "inicio_colheita": resultado.inicio_colheita,
+#                 "final_colheita": resultado.final_colheita,
+#                 "descricao_grao": resultado.descricao_grao,
+#                 "descricao_irrigacao": resultado.descricao_irrigacao,
+#                 "descricao_producao": resultado.descricao_producao,
+#             }
+#             lista_resultados.append(resultado_dict)
+
+#         #  Fechar a conexão
+#         conn.close()
+
+#         return jsonify(lista_resultados), 200
+
+#     except Exception as e:
+#         # Tratamento de erro: retorna uma mensagem de erro genérica em caso de exceção
+
+#         return jsonify({'error': 'Ocorreu um erro no processamento da solicitação.'}), 500
 
 
 # main
