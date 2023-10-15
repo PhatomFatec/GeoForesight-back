a0d685fef76e6ae5d4d3901271f04675eadd9a89
Lucas Guiraldelli <64923139+LucasGuiraldelli@users.noreply.github.com>
2023-10-15 20:04:29 -0300

    Update app.py

--- myapp/app.py
+++ myapp/app.py
@@ -17,7 +17,7 @@ from flask_jwt_extended import (
 
 app = Flask(__name__)
 
-app.config['JWT_SECRET_KEY'] = 'your-secret-key'
+# app.config['JWT_SECRET_KEY'] = 'your-secret-key'
 
 jwt = JWTManager(app)
 
