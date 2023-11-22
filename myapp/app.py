69764159edf560ec88802e0da723781e68e94694
lara-leal <lara.leal@fatec.sp.gov.br>
2023-11-22 19:45:22 -0300

    .

--- myapp/app.py
+++ myapp/app.py
@@ -63,122 +63,114 @@ db = SQLAlchemy(app)
 #############################
 
 
-# class Table(db.Model):
-#     tablename = 'table'
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(255), nullable=False)
-
-# class clima(db.Model):
-#     idClima = db.Column(db.Integer, primary_key=True, nullable=False)
-#     descricao = db.Column(db.String(255))
-#     operacao_credito_estadual = db.relationship(
-#         'operacao_credito_estadual', backref='clima')
+class clima(db.Model):
+    idClima = db.Column(db.Integer, primary_key=True, nullable=False)
+    descricao = db.Column(db.String(255))
+    operacao_credito_estadual = db.relationship(
+        'operacao_credito_estadual', backref='clima')
 
 
-# class irrigacao(db.Model):
-#     idirrigacao = db.Column(db.Integer, primary_key=True,
-#                             nullable=False,  autoincrement=False)
-#     descricao = db.Column(db.String(255))
-#     operacao_credito_estadual = db.relationship(
-#         'operacao_credito_estadual', backref='irrigacao')
+class irrigacao(db.Model):
+    idirrigacao = db.Column(db.Integer, primary_key=True,
+                            nullable=False,  autoincrement=False)
+    descricao = db.Column(db.String(255))
+    operacao_credito_estadual = db.relationship(
+        'operacao_credito_estadual', backref='irrigacao')
 
-# class solo(db.Model):
-#     idSolo = db.Column(db.Integer, primary_key=True, nullable=False)
-#     descricao = db.Column(db.String(255))
-#     operacao_credito_estadual = db.relationship(
-#         'operacao_credito_estadual', backref='solo')
-
-
-# class ciclo_producao(db.Model):
-#     idciclo = db.Column(db.Integer, primary_key=True,
-#                         nullable=False,  autoincrement=False)
-#     descricao = db.Column(db.String(255))
-#     operacao_credito_estadual = db.relationship(
-#         'operacao_credito_estadual', backref='ciclo_producao')
+class solo(db.Model):
+    idSolo = db.Column(db.Integer, primary_key=True, nullable=False)
+    descricao = db.Column(db.String(255))
+    operacao_credito_estadual = db.relationship(
+        'operacao_credito_estadual', backref='solo')
 
 
-# class grao(db.Model):
-#     idgrao = db.Column(db.Integer, primary_key=True,
-#                        nullable=False,  autoincrement=False)
-#     descricao = db.Column(db.String(255))
-#     operacao_credito_estadual = db.relationship(
-#         'operacao_credito_estadual', backref='grao')
+class ciclo_producao(db.Model):
+    idciclo = db.Column(db.Integer, primary_key=True,
+                        nullable=False,  autoincrement=False)
+    descricao = db.Column(db.String(255))
+    operacao_credito_estadual = db.relationship(
+        'operacao_credito_estadual', backref='ciclo_producao')
 
 
-# class evento_climatico(db.Model):
-#     idEvento = db.Column(db.Integer, primary_key=True, nullable=False)
-#     descricao = db.Column(db.String(255))
-#     operacao_credito_estadual = db.relationship(
-#         'operacao_credito_estadual', backref='evento_climatico')
+class grao(db.Model):
+    idgrao = db.Column(db.Integer, primary_key=True,
+                       nullable=False,  autoincrement=False)
+    descricao = db.Column(db.String(255))
+    operacao_credito_estadual = db.relationship(
+        'operacao_credito_estadual', backref='grao')
 
 
-# class produtos(db.Model):
-#     idproduto = db.Column(db.Integer, primary_key=True,
-#                           nullable=False,  autoincrement=False)
-#     nome = db.Column(db.String(255))
-#     Empreendimento = db.Relationship('empreendimento', backref='produtos')
+class evento_climatico(db.Model):
+    idEvento = db.Column(db.Integer, primary_key=True, nullable=False)
+    descricao = db.Column(db.String(255))
+    operacao_credito_estadual = db.relationship(
+        'operacao_credito_estadual', backref='evento_climatico')
 
 
-# class operacao_credito_estadual(db.Model):
-#     ref_bacen = db.Column(db.Integer, primary_key=True,
-#                           unique=True, nullable=False,  autoincrement=False)
-#     nu_ordem = db.Column(db.Integer, primary_key=True,
-#                          unique=True, nullable=False,  autoincrement=False)
-#     inicio_plantio = db.Column(db.Date, nullable=False)
-#     final_plantio = db.Column(db.Date, nullable=False)
-#     final_colheita = db.Column(db.Date, nullable=False)
-#     inicio_colheita = db.Column(db.Date, nullable=False)
-#     data_liberacao = db.Column(db.Date, nullable=False)
-#     data_vencimento = db.Column(db.Date, nullable=False)
-#     idciclo = db.Column(db.Integer, db.ForeignKey('ciclo_producao.idciclo'))
-#     idteste = db.Column(
-#         db.Integer, db.ForeignKey('empreendimento.idteste'))
-#     idClima = db.Column(db.Integer, db.ForeignKey('clima.idClima'))
-#     idEvento = db.Column(db.Integer, db.ForeignKey('evento_climatico.idEvento'))
-#     idgrao = db.Column(db.Integer, db.ForeignKey('grao.idgrao'))
-#     idSolo = db.Column(db.Integer, db.ForeignKey('solo.idSolo'))
-#     idirrigacao = db.Column(db.Integer, db.ForeignKey('irrigacao.idirrigacao'))
-#     idempreendimento = db.Column(
-#         db.Integer, db.ForeignKey('empreendimento.idempreendimento'))
-#     glebas = db.relationship('glebas', backref='operacao_credito_estadual')
-
-#     table_args = (
-#         db.PrimaryKeyConstraint('ref_bacen', 'nu_ordem'),
-#     )
+class produtos(db.Model):
+    idproduto = db.Column(db.Integer, primary_key=True,
+                          nullable=False,  autoincrement=False)
+    nome = db.Column(db.String(255))
+    Empreendimento = db.Relationship('empreendimento', backref='produtos')
 
 
-# class glebas(db.Model):
-#     idgleba = db.Column(db.Integer, primary_key=True,
-#                         nullable=False,  autoincrement=False)
-#     coordenadas = db.Column(
-#         Geography(geometry_type='POINT', srid=4326), nullable=False)
-#     altitude = db.Column(db.Double)
-#     nu_ponto = db.Column(db.Integer, nullable=False)
-#     ref_bacen = db.Column(db.Integer, db.ForeignKey(
-#         'operacao_credito_estadual.ref_bacen'))
-#     nu_ordem = db.Column(db.Integer, db.ForeignKey(
-#         'operacao_credito_estadual.nu_ordem'))
+class operacao_credito_estadual(db.Model):
+    ref_bacen = db.Column(db.Integer, primary_key=True,
+                          unique=True, nullable=False,  autoincrement=False)
+    nu_ordem = db.Column(db.Integer, primary_key=True,
+                         unique=True, nullable=False,  autoincrement=False)
+    inicio_plantio = db.Column(db.Date, nullable=False)
+    final_plantio = db.Column(db.Date, nullable=False)
+    final_colheita = db.Column(db.Date, nullable=False)
+    inicio_colheita = db.Column(db.Date, nullable=False)
+    data_liberacao = db.Column(db.Date, nullable=False)
+    data_vencimento = db.Column(db.Date, nullable=False)
+    idciclo = db.Column(db.Integer, db.ForeignKey('ciclo_producao.idciclo'))
+    idteste = db.Column(
+        db.Integer, db.ForeignKey('empreendimento.idteste'))
+    idClima = db.Column(db.Integer, db.ForeignKey('clima.idClima'))
+    idEvento = db.Column(db.Integer, db.ForeignKey('evento_climatico.idEvento'))
+    idgrao = db.Column(db.Integer, db.ForeignKey('grao.idgrao'))
+    idSolo = db.Column(db.Integer, db.ForeignKey('solo.idSolo'))
+    idirrigacao = db.Column(db.Integer, db.ForeignKey('irrigacao.idirrigacao'))
+    idempreendimento = db.Column(
+        db.Integer, db.ForeignKey('empreendimento.idempreendimento'))
+    glebas = db.relationship('glebas', backref='operacao_credito_estadual')
+
+    table_args = (
+        db.PrimaryKeyConstraint('ref_bacen', 'nu_ordem'),
+    )
 
 
-# class empreendimento(db.Model):
-#     idteste = db.Column(db.Integer, primary_key=True, nullable=False)
-#     idempreendimento = db.Column(db.BigInteger, nullable=False)
-#     finalidade = db.Column(db.String(255))
-#     cesta = db.Column(db.String(255), nullable=False)
-#     modalidade = db.Column(db.String(255), nullable=False)
-#     idproduto = db.Column(db.Integer, db.ForeignKey(
-#         'produtos.idproduto'), nullable=False)
+class glebas(db.Model):
+    idgleba = db.Column(db.Integer, primary_key=True,
+                        nullable=False,  autoincrement=False)
+    coordenadas = db.Column(
+        Geography(geometry_type='POINT', srid=4326), nullable=False)
+    altitude = db.Column(db.Double)
+    nu_ponto = db.Column(db.Integer, nullable=False)
+    ref_bacen = db.Column(db.Integer, db.ForeignKey(
+        'operacao_credito_estadual.ref_bacen'))
+    nu_ordem = db.Column(db.Integer, db.ForeignKey(
+        'operacao_credito_estadual.nu_ordem'))
+
+
+class empreendimento(db.Model):
+    idteste = db.Column(db.Integer, primary_key=True, nullable=False)
+    idempreendimento = db.Column(db.BigInteger, nullable=False)
+    finalidade = db.Column(db.String(255))
+    cesta = db.Column(db.String(255), nullable=False)
+    modalidade = db.Column(db.String(255), nullable=False)
+    idproduto = db.Column(db.Integer, db.ForeignKey(
+        'produtos.idproduto'), nullable=False)
     
 
-# class cooperados(db.Model):
-#      ref_bacen = db.Column(db.Integer, primary_key=True,unique=True, nullable=False,  autoincrement=False)
-#      nu_ordem = db.Column(db.Integer, primary_key=True,unique=True, nullable=False,  autoincrement=False)
-#      valor_parcela = db.Column(db.Double)
-#      cpf = db.Column(db.String(255))
+class cooperados(db.Model):
+     ref_bacen = db.Column(db.Integer, primary_key=True,unique=True, nullable=False,  autoincrement=False)
+     nu_ordem = db.Column(db.Integer, primary_key=True,unique=True, nullable=False,  autoincrement=False)
+     valor_parcela = db.Column(db.Double)
+     cpf = db.Column(db.String(255))
 
-# puxar id do user pelo email fazendo uma query
-# mudar a função de aceite para
-# 
 
 
 
